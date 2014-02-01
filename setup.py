7be8e7c62bb6f9320ed83501987be1c77c6f9d8c
Lennart Regebro <regebro@gmail.com>
2014-02-01 14:55:45 +0100

    Tested with Python 2.7. Works with 2.6 as well of course.

--- setup.py
+++ setup.py
@@ -32,8 +32,9 @@ setup(name='pas.plugins.memberapproval',
       # http://pypi.python.org/pypi?:action=list_classifiers
       classifiers=[
         'Framework :: Plone',
+        'Framework :: Plone :: 4.1',
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License (GPL)',
         ],
